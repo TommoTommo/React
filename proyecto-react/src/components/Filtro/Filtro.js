52578bebbb52ed3f26d96b7acde2a55fdadeba05
aguskrmn <acarman@udesa.edu.ar>
2023-09-11 16:08:01 -0300

    filtro

---
+++ proyecto-react/src/components/Filtro/Filtro.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Filtro extends Component{
+
+    constructor(props){
+        super(props);
+        this.state ={
+            textoDelInput: ''
+        }
+    }
+
+    controlarEnvio(evento){
+        evento.preventDefault();
+        console.log("No se mando");
+        return true
+    }
+
+    guardarDatosDelInput(eventoEnElInput){
+        this.setState({
+            textoDelInput: eventoEnElInput.target.value
+        }, () => this.props.filtrar(this.state.textoDelInput))
+
+
+        return true
+    }
+
+    render(){
+        return(
+            <form action="" method='GET' onSubmit={(e) => this.controlarEnvio(e)}>
+                <label htmlFor="">Texto a filtrar: </label>
+                <input type="text" name='filtro' onChange={(e)=>this.guardarDatosDelInput(e)} value={this.state.textoDelInput} />
+                <button type='submit'>Filtrar</button>
+            </form>
+        )
+    }
+
+}
+
+export default Filtro
+
--- proyecto-react/src/screens/Home/Home.js
+++ proyecto-react/src/screens/Home/Home.js
@@ -1,7 +1,11 @@
+import ListaPersonajes from "../../components/ListaPersonajes/ListaPersonajes";
+
 function Home(){
 
     return(
+        <section>
         <h1>Home</h1>
+        </section>
     )
 }
 
