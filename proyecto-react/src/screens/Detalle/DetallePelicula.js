862651096ac350ef11e34727e427fce0ae2654d6
aguskrmn <acarman@udesa.edu.ar>
2023-09-14 20:14:38 -0300

    detalle series

--- proyecto-react/src/screens/Detalle/DetallePelicula.js
+++ proyecto-react/src/screens/Detalle/DetallePelicula.js
@@ -4,61 +4,59 @@ const imagen = "https://image.tmdb.org/t/p/w342";
 
 
 class DetallePelicula extends Component {
-constructor(props){
-    super(props);
-    this.state = {
-        pelicula: null, // Cambiado de un arreglo a un objeto único
-      cargando: true,
+    constructor(props) {
+        super(props);
+        this.state = {
+            pelicula: null
+        }
     }
-}
 
-componentDidMount() {
-    //busco el id de la pelicula en cuestion
-let peliculaId = this.props.match.params.id;
+    componentDidMount() {
+        //busco el id de la pelicula en cuestion
+        let peliculaId = this.props.match.params.id;
 
-    // fetch de peliculas
-    fetch(`https://api.themoviedb.org/3/movie/${peliculaId}?api_key=f2acabc2f1f7dfa29f6493c2fcca003f`) //peliculas populares
-        .then((response) => response.json())
-        .then((data) => {
-            this.setState({
-                pelicula: data,
-                cargando: false,
+        // fetch de peliculas
+        fetch(`https://api.themoviedb.org/3/movie/${peliculaId}?api_key=f2acabc2f1f7dfa29f6493c2fcca003f`) //peliculas populares
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    pelicula: data,
+                })
             })
-        })
-        .catch(e => console.log(e))
+            .catch(e => console.log(e))
 
     }
 
-render(){
-    return(
+    render() {
+        return (
             <main>
                 {this.state.pelicula ?
-                ( <React.Fragment>
-                <h2>Titulo: {this.state.pelicula.title}</h2>
-                <section>
-                    <article>
-                    <img src={imagen + this.state.pelicula.poster_path} alt={this.state.pelicula.title} />
-                    </article>
+                    (<React.Fragment>
+                        <h2>Titulo: {this.state.pelicula.title}</h2>
+                        <section>
+                            <article>
+                                <img src={imagen + this.state.pelicula.poster_path} alt={this.state.pelicula.title} />
+                            </article>
 
-                    <article>
-                        <ul>
-                            <li> <h3>Calificación/ raiting:</h3> {this.state.pelicula.vote_average}</li> 
-                            <br></br>     
-                            <li> <h3>Generos:</h3> {this.state.pelicula.genres.map((genre, idx)=> <li>{genre.name}</li>)} </li>
-                            <br></br>
-                            <li><h3>Fecha de estreno:</h3> {this.state.pelicula.release_date}</li>
-                            <br></br>
-                            <p><h3>Sinópsis:</h3>   {this.state.pelicula.overview}</p>
-                        </ul>
-                    </article>
-                </section>
-            </React.Fragment> 
-          ) : (
-            <h3>Cargando..</h3>
-          )}
-        </main>
-      );
-  }
+                            <article>
+                                <ul>
+                                    <li> <h3>Calificación/ raiting:</h3> {this.state.pelicula.vote_average}</li>
+                                    <br></br>
+                                    <li> <h3>Generos:</h3> {this.state.pelicula.genres.map((genre, idx) => <li>{genre.name}</li>)} </li>
+                                    <br></br>
+                                    <li><h3>Fecha de estreno:</h3> {this.state.pelicula.release_date}</li>
+                                    <br></br>
+                                    <p><h3>Sinópsis:</h3>   {this.state.pelicula.overview}</p>
+                                </ul>
+                            </article>
+                        </section>
+                    </React.Fragment>
+                    ) : (
+                        <h3>Cargando..</h3>
+                    )}
+            </main>
+        );
+    }
 }
 
 
---
+++ proyecto-react/src/screens/Detalle/DetalleSerie.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import './Detalle.css';
+const imagen = "https://image.tmdb.org/t/p/w342";
+
+class DetalleSerie extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            series: null 
+        }
+    }
+
+
+    componentDidMount() {
+        //busco el id de la serie en cuestion
+        let serieId = this.props.match.params.id;
+
+        // fetch de peliculas
+        fetch(`https://api.themoviedb.org/3/tv/${serieId}?api_key=f2acabc2f1f7dfa29f6493c2fcca003f`) //peliculas populares
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    series: data,
+                })
+            })
+            .catch(e => console.log(e))
+
+    }
+    render() {
+        return (
+            <main>
+                {this.state.series ?
+                    (<React.Fragment>
+                        <h2>Titulo: {this.state.series.title}</h2>
+                        <section>
+                            <article>
+                                <img src={imagen + this.state.series.poster_path} alt={this.state.series.title} />
+                            </article>
+
+                            <article>
+                                <ul>
+                                    <li> <h3>Calificación/ raiting:</h3> {this.state.series.vote_average}</li>
+                                    <br></br>
+                                    <li> <h3>Generos:</h3> {this.state.series.genres.map((genre, idx) => <li>{genre.name}</li>)} </li>
+                                    <br></br>
+                                    <li><h3>Fecha de estreno:</h3> {this.state.series.release_date}</li>
+                                    <br></br>
+                                    <p><h3>Sinópsis:</h3>   {this.state.series.overview}</p>
+                                </ul>
+                            </article>
+                        </section>
+                    </React.Fragment>
+                    ) : (
+                        <h3>Cargando..</h3>
+                    )}
+            </main>
+        );
+    }
+}
+
+export default DetalleSerie;
